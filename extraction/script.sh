48c57e47104923a76ba7b7d853d54bd260ea9447
Antoine Blanot <91732614+AntoineBlanot@users.noreply.github.com>
2023-07-10 16:47:34 +0900

    Delete script.sh

--- extraction/script.sh
+++
@@ -1,9 +0,0 @@
-python eval.py \
-    -m AntoineBlanot/roberta-large-squadv2 \
-    -d /home/chikara/Documents/zero-nlu/data/extraction/chat-gpt/favorite-continent-question.json \
-    -e fav_continent
-
-# python eval.py \
-#     -m /home/chikara/tmp/qa/large_lora_squad_v2 --lora \
-#     -d /home/chikara/Documents/zero-nlu/data/extraction/chat-gpt/favorite-continent-question.json \
-#     -e fav_continent
