e29bbfa7188ea62ed00c900efec41a5cdb162ca5
Antoine BLANOT <antoineblanot@gmail.com>
2023-07-11 19:51:53 +0900

    add no_class with treshold

--- classification/models.py
+++ classification/models.py
@@ -6,9 +6,37 @@ from peft import PeftModel, PeftConfig
 from utils import prepare_features, postprocess_predictions
 
 LABEL_MAPPING = {
-    "positive": "The document expresses a sentiment of positivity.",
-    "negative": "The document expresses a sentiment of negativity.",
-    "neutral": "The document expresses a sentiment of neutrality."
+    "watch-in-person": "I like to watch where the action is.",
+    "watches-on-tv": "I like to watch on the television.",
+
+    "natural-wonders": "I like nature.",
+    "man-made-monuments-answer": "I like monuments.",
+
+    "topic-books-physical-books": "I like physical books.",
+    "topic-books-ebooks": "I like electronic books.",
+
+    "topic-books-most-sold-book-rowling": "It is J. K. Rowling.",
+    "topic-books-most-sold-book-tolkien": "It is J. R. R. Tolkien.",
+
+    "topic-food-for-breakfast": "It is for breakfast.",
+    "topic-food-for-lunch": "It is for lunch.",
+    "topic-food-for-dinner": "It is for dinner.",
+
+    "topic-hometown-type-of-building-apartment-answer": "I live in an apartment.",
+    "topic-hometown-type-of-building-house-answer": "I live in a house.",
+
+    # "topic-pet-eat-answer-little": "my pet eats a little",
+    # "topic-pet-eat-answer-lots": "my pet eats a lot",
+
+    "topic-speaker-age-less-than-18-answer": "I am less than 18 years old.",
+    "topic-speaker-age-greater-than-18-answer": "I am more than 18 years old.",
+
+    "topic-travel-homecountry-favorite-hemisphere-north": "I like the North.",
+    "topic-travel-homecountry-favorite-hemisphere-south": "I like the South.",
+
+    # "positive": "The document expresses a sentiment of positivity.",
+    # "negative": "The document expresses a sentiment of negativity.",
+    # "neutral": "The document expresses a sentiment of neutrality."
 }
 
 class BaseClassifier():
@@ -21,6 +49,7 @@ class BaseClassifier():
         """
         Classify the document into one of the labels.
         """
+        threshold = 2 / (len(labels) + 1)
         candidates = [self.label_mapping.get(label, f"The document is about {label}.") for label in labels]
         features = prepare_features(document=document, candidates=candidates, tokenizer=self.tokenizer)
         inputs = self.prepare_inputs(features=features)
@@ -29,8 +58,8 @@ class BaseClassifier():
         predictions = postprocess_predictions(predictions=outputs.logits.cpu().numpy())
 
         results = dict(
-            label=labels[predictions['prediction']['idx']],
-            score=predictions['prediction']['score']
+            label='' if no_class and (predictions['prediction']['score'] < threshold) else labels[predictions['prediction']['idx']],
+            score=float(predictions['prediction']['score'])
         )
 
         return results
